249f4080648bff712dbcb82279315e9e99625886
LapaKWsb <128227172+LapaKWsb@users.noreply.github.com>
2023-06-09 18:13:13 +0200

    Add files via upload

---
+++ Obwód koła.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace Zadanie2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj pole koła");
+            double pole = Convert.ToDouble(Console.ReadLine());
+
+            double promien = Math.Sqrt(pole / Math.PI);
+
+            double obwod = 2 * Math.PI * promien;
+
+            Console.WriteLine("Obwód koła wynosi " + obwod);
+        }
+    }
+}
