bc040685fe1dfe82556eeed962744e642ff45dea
LapaKWsb <128227172+LapaKWsb@users.noreply.github.com>
2023-06-09 18:14:15 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace Zadanie3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj wagę w kg:");
+            double waga = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Podaj wzrost w metrach:");
+            double wzrost = Convert.ToDouble(Console.ReadLine());
+
+            double bmi = waga / (wzrost * wzrost);
+
+            Console.WriteLine("Twoje BMI wynosi: " + bmi);
+        }
+    }
+}
