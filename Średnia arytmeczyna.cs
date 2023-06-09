13798b3ebc3649becfcb7ff1a580fc2be9093194
LapaKWsb <128227172+LapaKWsb@users.noreply.github.com>
2023-06-09 23:20:14 +0200

    Średnia arytmeczyna.cs

---
+++ Średnia arytmeczyna.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace Zadanie1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Wpisz 3 liczby");
+
+            double liczba1 = double.Parse(Console.ReadLine());
+            double liczba2 = double.Parse(Console.ReadLine());
+            double liczba3 = double.Parse(Console.ReadLine());
+
+            double srednia = (liczba1 + liczba2 + liczba3) / 3;
+
+            Console.WriteLine("Średnia arytmetyczna: " + srednia);
+        }
+    }
+}
