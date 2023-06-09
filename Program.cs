9bc7e4a8cb163f45d461a003d6ccc732d8c342a5
LapaKWsb <128227172+LapaKWsb@users.noreply.github.com>
2023-06-09 21:56:37 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace Zadanie4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj współrzędne punktu P (x, y):");
+            double x = Convert.ToDouble(Console.ReadLine());
+            double y = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Podaj promień koła r:");
+            double r = Convert.ToDouble(Console.ReadLine());
+
+            double distanceSquared = x * x + y * y;
+
+            if (distanceSquared <= r * r)
+            {
+                Console.WriteLine("Punkt P leży w obrębie koła.");
+            }
+            else
+            {
+                Console.WriteLine("Punkt P nie leży w obrębie koła.");
+            }
+        }
+    }
+}
